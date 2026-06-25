241d23a38f30cc406eb0d3cd9c696567ab485b36
somjdev <110367081+somjdev@users.noreply.github.com>
2026-06-25 13:22:33 +1200

    feat: incremental generation, cash per second

---
+++ index.js
@@ -0,0 +1,64 @@
+let incrementals = 0;
+let avaliableCash = 0;
+
+// Business
+let unsoldIncrementals = 0;
+let incrementalCost = 30;
+let sellChance = 0.69;
+
+// Manufacturing
+let avaliableParts = 20;
+let partsCost = 14;
+
+const randomNumber = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+};
+
+const to2DP = (number) => {
+    return (Math.round(number * 100) / 100).toFixed(2);
+}
+
+
+setInterval(() => sellIncremental(), sellInterval());
+
+generatePartPrice();
+setInterval(() => generatePartPrice(), randomNumber(1000, 10000));
+
+function updateIncrementals() {
+    if (avaliableParts > 0) {
+        avaliableParts--;
+        incrementals++;
+        unsoldIncrementals++;
+    }
+    document.getElementById("incrementals").textContent = incrementals;
+    document.getElementById("unsoldAmount").textContent = unsoldIncrementals;
+    document.getElementById("avaliableParts").textContent = avaliableParts;
+}
+
+
+function sellIncremental() {    
+    if (unsoldIncrementals > 0) {
+        unsoldIncrementals--;
+        avaliableCash += incrementalCost;
+        document.getElementById("unsoldAmount").textContent = unsoldIncrementals;
+        document.getElementById("cash").textContent = avaliableCash / 100;
+        document.getElementById("avgCashPerSec").textContent = strip((incrementalCost / 100) / (sellInterval() / 1000));
+    } else {
+        document.getElementById("avgCashPerSec").textContent = to2DP(0);
+    }
+
+}
+
+function generatePartPrice() {
+    partsCost = randomNumber(10, 30) * 100;
+    document.getElementById("partsCost").textContent = partsCost / 100;
+}
+
+
+function strip(number) {
+    return (parseFloat(number).toPrecision(2));
+}
+
+function sellInterval() {
+    return strip(1000 / sellChance);
+}
